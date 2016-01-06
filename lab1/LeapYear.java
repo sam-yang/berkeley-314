ff6771ba6e4d2ed5952acb1b720610876f7ff83e
Samuel Yang <sam.yang@utexas.edu>
2016-01-05 22:14:17 -0500

    Completed LeapYear.java

---
+++ lab1/LeapYear.java
@@ -0,0 +1,14 @@
+public class LeapYear {
+	public static boolean isLeapYear(int year) {
+		if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
+			return true;
+		return false;
+	}
+	public static void main (String[] args) {
+		int year = 2000;
+		if (isLeapYear(year))
+			System.out.println(year + " is a leap year.");
+		else
+			System.out.println(year + " is not a leap year.");
+	}
+}
